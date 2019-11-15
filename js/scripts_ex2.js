2f037c46636ea3d4d6aefe68db0b46b5c55ecbec
BiggsBottor <jonatan.lledo@gmail.com>
2019-11-15 12:35:23 +0100

    leaflet ex2 FASE 1, añadir un marcador mostrando las coordenadas aplicando max zoom

---
+++ js/scripts_ex2.js
@@ -0,0 +1,22 @@
+var zoomLevel = 16;
+var maxZoom = 18;
+var plazaCatalunya = new L.LatLng(41.3870154, 2.1700471);
+
+var map = L.map('mapid').setView(plazaCatalunya, zoomLevel);
+
+// Capas base
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap<\/a> contributors',
+    minZoom: 10,
+    maxZoom: maxZoom
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    map.setView(e.latlng, maxZoom);
+    let marker = L.marker(e.latlng).addTo(map);
+    marker.bindPopup(`<h6 class="h6"><b>Mis coordenadas son:</b></h6><br> Lat:${e.latlng.lat}, Long:${e.latlng.lng}`).openPopup();
+}
+
+map.on('click', onMapClick);
